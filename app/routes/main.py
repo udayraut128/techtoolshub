a88b49aca7a32166bcd3a97875f37aea6f12c5c9
Uday Raut <rautuday75@gmail.com>
2026-04-24 00:22:27 +0530

    add robotand stie

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, send_from_directory, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
     return render_template("index.html")
+
+
+@main.route("/robots.txt")
+def robots():
+    return send_from_directory("static", "robots.txt")
+
+
+@main.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory("static", "sitemap.xml")
